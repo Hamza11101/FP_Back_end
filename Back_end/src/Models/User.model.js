dea7867b4c57457b57be397ff8cc5a7bf6530517
Hamza Boubaker <hamzaboubaker21@gmail.com>
2022-05-23 11:47:49 +0200

    First Commit Front

--- Back_end/src/Models/User.model.js
+++ Back_end/src/Models/User.model.js
@@ -3,17 +3,19 @@ const Schema = mongoose.Schema;
 
 
 const UserSchema = new Schema({
-    FirstName:{type:String,
+    firstName:{type:String,
     required:[true,'First Name field is required']},
-    LastName:{type:String,
+    lastName:{type:String,
         required:[true,'Last Name field is required']},
-    Email:{type:String,
+    email:{type:String,
         required:[true,'Email Name field is required']},
-    Password:{type:String,
+    password:{type:String,
         required:[true,'Password Name field is required']},
-    Role:{type:String,
+    role:{type:String,
         required:[true,'Role field is required']},
-
+},{
+    timestamps: true,
+    versionKey: false
 });
 
 const User = mongoose.model('user',UserSchema);
